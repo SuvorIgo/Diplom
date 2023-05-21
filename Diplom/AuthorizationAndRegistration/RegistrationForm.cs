3c7db217605a2e8c00da4a7b088fe3259dff9747
Igor Suvorov <suekandig@mail.ru>
2023-05-21 03:23:53 +0300

    Добавил класс ApplicationContext

--- Diplom/AuthorizationAndRegistration/RegistrationForm.cs
+++ Diplom/AuthorizationAndRegistration/RegistrationForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Diplom.libs.crypt;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
---
+++ Diplom/libs/db/ApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using Diplom.libs.db.entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplom.libs.db
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; } = null!;
+        public DbSet<Drivers> Drivers { get; set; } = null!;
+        public DbSet<Storages> Storages { get; set; } = null!;
+        public DbSet<Transports> Transports { get; set; } = null!;
+        public DbSet<Orders> Orders { get; set; } = null!;
+        public DbSet<Products> Products { get; set; } = null!;
+        public DbSet<Transportations> Transportations { get; set; } = null!;
+        public DbSet<TransportsDrivers> TransportsDrivers { get; set; } = null!;
+
+        public ApplicationContext() 
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=DiplomDB;Trusted_Connection=True;");
+        }
+    }
+}
